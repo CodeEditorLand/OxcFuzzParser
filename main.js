117aec39b8b9156d7e13facdfb06b2c649e6c46b
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-05-26 14:19:46 +0300

    squash!

--- main.js
+++ main.js
@@ -1,31 +1,28 @@
-'use strict';
+const { fuzz } = require("./fuzz");
 
-let { fuzz } = require('./fuzz');
-
-let fs = require('fs');
-let { exec } = require('child_process');
-let binary = process.argv[2]
+const fs = require("fs");
+const { exec } = require("child_process");
+const binary = process.argv[2];
 
 if (!binary) {
-  console.log("Please provide a binary.")
-  return
+	console.log("Please provide a binary.");
+	return
 }
 
-let file = '__tmp.js'
-let fn = src => {
-  fs.writeFileSync(file, src, 'utf8');
-  return new Promise((res, rej) => {
-    exec(binary + ' ' + file, {}, (err, _stdout, stderr) => {
-      if (err == null) {
-        res();
-      } else {
-        rej(new Error(stderr));
-      }
-    });
-  });
+const file = "__tmp.js";
+const fn = (src) => {
+	fs.writeFileSync(file, src, "utf8");
+	return new Promise((res, rej) => {
+		exec(binary + " " + file, {}, (err, _stdout, stderr) => {
+			if (err == null) {
+				res();
+			} else {
+				rej(new Error(stderr));
+			}
+		});
+	});
 };
 
-let known = [
-];
+const known = [];
 
 fuzz(fn, 200_000, known);
